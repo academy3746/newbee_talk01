2b7df9a4b190db93b889cbc75518ad22ea23870a
code_otaku <academy3746@gmail.com>
2024-03-25 21:17:50 +0900

    채팅방 목록 출력 함수 추가 작업

--- lib/common/utils/supabase_service.dart
+++ lib/common/utils/supabase_service.dart
@@ -1,4 +1,8 @@
+import 'dart:async';
+
+import 'package:newbee_talk/features/auth/models/chat_message.dart';
 import 'package:newbee_talk/features/auth/models/member.dart';
+import 'package:newbee_talk/features/main/models/chat_room.dart';
 import 'package:supabase_flutter/supabase_flutter.dart';
 
 class SupabaseService {
@@ -18,6 +22,45 @@ class SupabaseService {
         );
 
     return userMap.map((data) => MemberModel.fromMap(data)).toList();
+  }
+
+  /// 채팅방 목록 출력
+  Future<List<ChatMessageModel>> fetchChatRooms() async {
+    List<ChatMessageModel> res = [];
+
+    final roomMap = await supabase.from('chat_room').select();
+
+    List<ChatRoomModel> roomList = roomMap
+        .map(
+          (data) => ChatRoomModel.fromJson(data),
+        )
+        .toList();
+
+    for (var room in roomList) {
+      final messageMap = await supabase
+          .from('chat_message')
+          .select()
+          .eq(
+            'chat_room_id',
+            room.idx!,
+          )
+          .order(
+            'created_at',
+            ascending: true,
+          );
+
+      if (messageMap.isEmpty) continue;
+
+      List<ChatMessageModel> messages = messageMap
+          .map(
+            (data) => ChatMessageModel.fromJson(data),
+          )
+          .toList();
+
+      res.add(messages.last);
+    }
+
+    return res;
   }
 
   /// 로그인 유저 UID 획득
