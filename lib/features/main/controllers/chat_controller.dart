9e752242010291afb4358526ac693a9cfe622b37
code_otaku <academy3746@gmail.com>
2024-04-05 23:11:25 +0900

    :white_check_mark: Chat Message Model 구독 on Chat Controller

--- lib/features/main/controllers/chat_controller.dart
+++ lib/features/main/controllers/chat_controller.dart
@@ -1,5 +1,6 @@
 import 'package:get/get.dart';
 import 'package:newbee_talk/common/constants/date.dart';
+import 'package:newbee_talk/features/auth/models/chat_message.dart';
 import 'package:newbee_talk/features/auth/models/member.dart';
 import 'package:newbee_talk/features/main/models/chat_room.dart';
 
@@ -9,8 +10,14 @@ class ChatCont extends GetxController {
   /// Instances Model Classes
   final _records = Rx<(ChatRoomModel, MemberModel)?>(null);
 
+  /// Instances Chat Message Model Class
+  final _msg = Rxn<ChatMessageModel>();
+
   /// Getter (ChatRoomModel, MemberModel)
   (ChatRoomModel, MemberModel)? get records => _records.value;
+
+  /// Getter (_msg)
+  ChatMessageModel? get msg => _msg.value;
 
   /// Setter (ChatRoomModel, MemberModel)
   void setRecords((ChatRoomModel, MemberModel) model) {
